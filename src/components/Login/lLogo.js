f3bc43722fbf232ad0fa0632d0bebd3d95fee028
Mersharch <mersharchdemenya@icloud.com>
2021-10-18 13:31:30 +0000

    first

---
+++ src/components/Login/lLogo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function lLogo() {
+    return (
+        <div>
+            <img src="HQGF2574.JPG" alt="logo" className="logo" />
+        </div>
+    );
+}
+
+export default lLogo;
