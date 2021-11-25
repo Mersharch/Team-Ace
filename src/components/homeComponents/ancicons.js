e7f5dcc6e81c796e62cdea94e945798b18702c97
Mersharch <mersharchdemenya@icloud.com>
2021-11-25 05:48:11 +0000

    correct classNames

--- src/components/homeComponents/ancicons.js
+++ src/components/homeComponents/ancicons.js
@@ -1,5 +1,4 @@
 import { AiOutlineUser } from "react-icons/ai";
-import { AiOutlineShoppingCart } from "react-icons/ai";
 import '../../styles/ancicons.css'
 
 
@@ -9,30 +8,30 @@ function AnCIcons(){
             <div className="dropdown">
                 <AiOutlineUser size="23px" className="dropdown-trigger" style={{ marginRight: "35px", }} />
                 <div className="dropdown-menu">
-                    <div class="dropdown-content">
-                        <a href="#" class="dropdown-item">
+                    <div className="dropdown-content">
+                        <a href="/" className="dropdown-item">
                             Overview
                         </a>
-                        <a href="#" class="dropdown-item">
+                        <a href="/" className="dropdown-item">
                             Modifiers
                         </a>
-                        <a href="#" class="dropdown-item">
+                        <a href="/" className="dropdown-item">
                             Grid
                         </a>
-                        <a href="#" class="dropdown-item">
+                        <a href="/" className="dropdown-item">
                             Form
                         </a>
-                        <a href="#" class="dropdown-item">
+                        <a href="/" className="dropdown-item">
                             Elements
                         </a>
-                        <a href="#" class="dropdown-item">
+                        <a href="/" className="dropdown-item">
                             Components
                         </a>
-                        <a href="#" class="dropdown-item">
+                        <a href="/" className="dropdown-item">
                             Layout
                         </a>
-                        <hr class="dropdown-divider" />
-                        <a href="#" class="dropdown-item">
+                        <hr className="dropdown-divider" />
+                        <a href="/" className="dropdown-item">
                             Sign Out
                         </a>
                     </div>
--- src/components/homeComponents/fp.js
+++ src/components/homeComponents/fp.js
@@ -9,7 +9,7 @@ function Fp() {
             <div className="images">
 
                 <img src="https://images.unsplash.com/photo-1580910530751-a4d68d339cb4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjZ8fGxhZGllcyUyMGNsb3RoZXN8ZW58MHwxfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="image1" className="fpimg1" />
-                <img src="https://images.unsplash.com/photo-1564742191034-961da6c0e088?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTd8fGhhaXJ8ZW58MHwxfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" className="fpimg2"/>
+                <img src="https://images.unsplash.com/photo-1564742191034-961da6c0e088?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTd8fGhhaXJ8ZW58MHwxfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" className="fpimg2" alt="fpimg4" />
                 <img src="https://images.unsplash.com/photo-1600102427329-d5b2cde7e162?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGRyZXNzfGVufDB8MXwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="image3" className="fpimg3" />
 
             </div>
