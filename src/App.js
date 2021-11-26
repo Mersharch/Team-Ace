cd39e6c311542172b805860b4d3a5e7cb54d7ca7
DavidVan-pee <91279056+DavidVan-pee@users.noreply.github.com>
2021-11-26 21:53:30 +0000

    new about page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import './App.css';
-import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
-<<<<<<< HEAD
-import Home from './pages/home'
-import About from './pages/about';
-=======
+import { BrowserRouter as Router, Routes , Route } from 'react-router-dom'
+
 import Home from './pages/home';
 import About from './pages/about';
 import Contact from './pages/contact';
 import LogIn from './pages/login';
 import SignUp from './pages/signup';
 
-
-
-
->>>>>>> eb5b6455f0ee0482328bdc19399e03b8eab073d2
 
 
 function App() {
@@ -23,14 +16,13 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/about" element={<About />} />
-<<<<<<< HEAD
-=======
+
           <Route exact path="/contact" element={<Contact />} />
           <Route exact path="/signin" element={<LogIn />} />
           <Route exact path="/signup" element={<SignUp />} />
 
 
->>>>>>> eb5b6455f0ee0482328bdc19399e03b8eab073d2
+
 
         </Routes>
       </div>
--- src/pages/about.js
+++ src/pages/about.js
@@ -11,13 +11,13 @@ function About() {
         <h1 style={{color:"#000"}}>ABOUT US</h1>
         
          <body>
-          Founded in 2018 "Just Hair and Fashion Wear" is a business enterprise aimed at providing women and young girls the necessary confidence to feel good in any kind of clothes they wear. 
-          Our clothes are of the highest quality and leaves customers feeling <b>beautiful and confident</b>.
+          Founded in 2018 "Just Hair and Fashion Wear" is a business enterprise aimed at providing women and young girls the necessary confidence to feel good in any kind of clothes or hairstyle of their choice. 
+          Our products are of the highest quality and leaves customers feeling <b>beautiful and confident</b>. Our products are affordable and accessible to all.
         </body> 
 
         <h2 style={{color:"#000"}}>VISION STATEMENT</h2>
 
-         <body>The vision of Just Hair and Fashion Wear is to bring out the beauty in our customers because the first step in loving ourselves is loving how we look, and we thrive in making sure that the objective is established.</body>
+         <body>The vision of Just Hair and Fashion Wear is to bring out the beauty in our customers because the first step in loving ourselves is loving how we look, and we thrive in making sure that the objective is reached.</body>
 
         <img src="https://t3.ftcdn.net/jpg/03/14/09/50/360_F_314095077_iJtVBwPTvbd1fdJIKi6Sub3XNkT5Qnfy.jpg" className="nicepic"/>
         <Footer />
