bc4ef6774e243fc6abfaa84d34507241266580dd
Mersharch <mersharchdemenya@icloud.com>
2021-11-27 00:00:32 +0000

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import './App.css';
 import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
-<<<<<<< HEAD
-import Home from './pages/home'
-import About from './pages/about';
-=======
 import Home from './pages/home';
 import About from './pages/about';
 import Contact from './pages/contact';
@@ -13,7 +9,6 @@ import SignUp from './pages/signup';
 
 
 
->>>>>>> eb5b6455f0ee0482328bdc19399e03b8eab073d2
 
 
 function App() {
@@ -23,14 +18,12 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/about" element={<About />} />
-<<<<<<< HEAD
-=======
           <Route exact path="/contact" element={<Contact />} />
           <Route exact path="/signin" element={<LogIn />} />
           <Route exact path="/signup" element={<SignUp />} />
+          
 
 
->>>>>>> eb5b6455f0ee0482328bdc19399e03b8eab073d2
 
         </Routes>
       </div>
