8b1b3df235c6704817f8a1a97c38eb1a84894cfe
Yeboah-NK <yebshimself01@gmail.com>
2021-11-27 20:03:18 +0000

    thank you

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Home from './pages/home';
+import Thankyou from './pages/Thankyou';
 
 
 
 function App() {
   return (
     <div className="App">
-      <Home />
+
     </div>
   );
 }
