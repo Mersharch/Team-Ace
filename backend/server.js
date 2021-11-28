e609da61d6c791080574bcc5967897a3b32ea0e3
Mersharch <mersharchdemenya@icloud.com>
2021-11-28 20:01:43 +0000

    started backend

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+//requiring express
+const express = require("express");
+
+//express app
+const app = express();
+
+//definign port
+const port = process.env.PORT || 5000;
+
+//listening to port
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
+
