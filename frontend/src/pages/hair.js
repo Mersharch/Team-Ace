d6d04f5fca0920d488f26e9c3e21c79ffd7b8af3
Mersharch <mersharchdemenya@icloud.com>
2021-11-30 21:56:01 +0000

    build not working fixing

--- frontend/src/pages/hair.js
+++ frontend/src/pages/hair.js
@@ -1,15 +1,33 @@
-
+import { Link } from 'react-router-dom';
+import '../styles/hair.css';
+import Card from '../components/prods/card';
 
 
 
 
 function Hair() {
+
   return (
     <div className="wigs-div">
+      <div className="hsidebar">
         <h1 className="wigs">HAIR</h1>
-        <div className="wigs-container">
-              
+
+        <div className="hnav">
+          <ul className="hul">
+            <li className="hli"><Link to="#human-hair" className="hlinks">Human Hair</Link></li>
+            <li className="hli"><Link to="#wigs" className="hlinks">Wigs Hair</Link></li>
+            <li className="hli"><Link to="#Hair Bundle" className="hlinks">Hair Bundles</Link></li>
+
+          </ul>
         </div>
+
+      </div>
+
+      <div className="wigs-container">
+        <Card />
+
+        
+      </div>
     </div>
   );
 }
