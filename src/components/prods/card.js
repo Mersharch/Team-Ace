c7107e4930f1d3416ee387ef2620d33e8adc96b0
Mersharch <mersharchdemenya@icloud.com>
2021-12-13 00:39:24 +0800

    setting up cards

--- src/components/prods/card.js
+++ src/components/prods/card.js
@@ -1,14 +1,15 @@
 import '../../styles/card.css';
 
 
-function Card() {
+function Card(props) {
+    const item = props.item;
   return (
         <div className="card-container">
             <div className="prod-img">
             </div>
             <div className="prod-details">
-                <h2 className="prod-name">prod-name</h2>
-                <h3 className="prod-price">prod-price</h3>
+                <h2 className="prod-name">{item.name}</h2>
+                <h3 className="prod-price">{item.price}</h3>
             </div>
             <div className="atc-container">
                 <button className="atc-button">Add To Cart + </button>
